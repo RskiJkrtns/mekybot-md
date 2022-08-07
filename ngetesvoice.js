e8022ce516ba846e85a902198eedd8006a3e8ca4
RskiJkrtns <97654894+RskiJkrtns@users.noreply.github.com>
2022-08-07 23:04:13 +0700

    Update ngetesvoice.js

--- ngetesvoice.js
+++ ngetesvoice.js
@@ -1,11 +1,11 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let contoh = fs.readFileSync('./mp3/Bot.opus') 
+let contoh = fs.readFileSync('./mp3/bot.opus') 
 conn.sendFile(m.chat, contoh, '', '', m, true)
 }
 
-handler.customPrefix = /^(bot|robot)$/i
+handler.customPrefix = /^(bot|robot|p|tes|)$/i
 handler.command = new RegExp
 
 handler.limit = true
